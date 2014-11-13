db7abbcbfc762469c1c0b18964db1020c2fe15ac
Christopher Felton <chris.felton@gmail.com>
2014-11-12 21:31:37 -0600

    small testing modifications

--- hdl/jpegenc_v1/verilog/BUF_FIFO.v
+++ hdl/jpegenc_v1/verilog/BUF_FIFO.v
@@ -98,29 +98,32 @@ module BUF_FIFO
     parameter C_NUM_LINES = 8 + C_EXTRA_LINES;
     
     reg [15:0] 			 pixel_cnt = 0;
-    wire [15:0] line_cnt = 0;
-    wire [C_PIXEL_BITS - 1:0] ramq = 0;
-    reg [C_PIXEL_BITS - 1:0] ramd = 0;
+    wire [15:0] 		 line_cnt = 0;
+    wire [C_PIXEL_BITS - 1:0] 	 ramq = 0;
+    reg [C_PIXEL_BITS - 1:0] 	 ramd = 0;
     reg [$clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1:0] ramwaddr = 0;
     reg 					       ramenw = 1'b 0;
+    
     wire [$clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1:0] ramraddr = 0;
-    reg [3:0] 					       pix_inblk_cnt = 0;
-    reg [3:0] 				      pix_inblk_cnt_d1 = 0;
-    reg [2:0] 				      line_inblk_cnt = 0;
-    reg [12:0] 				      read_block_cnt = 0;
-    reg [12:0] 				      read_block_cnt_d1 = 0;
-    wire [12:0] 			      write_block_cnt = 0;
-    reg [16 + $clog2(C_NUM_LINES) 		      - 1:0] ramraddr_int = 0;
-    reg [16 + $clog2(C_NUM_LINES)		      - 1:0] raddr_base_line = 0;
-    reg [15:0] 						     raddr_tmp = 0;
-    reg [$clog2(C_MAX_LINE_WIDTH * C_NUM_LINES)  - 1:0] ramwaddr_d1 = 0;
-    wire [$clog2(C_NUM_LINES) 		      - 1:0] 	line_lock = 0;
-    reg [$clog2(C_NUM_LINES) 		      - 1:0] memwr_line_cnt = 0;
-    reg [$clog2(C_NUM_LINES) 		      - 1 + 1:0] memrd_offs_cnt = 0;
-    reg [$clog2(C_NUM_LINES) 		      - 1:0] 	 memrd_line = 0;
-    reg [15:0] 					     wr_line_idx = 0;
-    reg [15:0] 				      rd_line_idx = 0;
-    reg 				      image_write_end = 1'b 0;  
+    reg [3:0] 						pix_inblk_cnt = 0;
+    reg [3:0] 						pix_inblk_cnt_d1 = 0;
+    reg [2:0] 						line_inblk_cnt = 0;
+    reg [12:0] 						read_block_cnt = 0;
+    reg [12:0] 						read_block_cnt_d1 = 0;
+    wire [12:0] 					write_block_cnt = 0;
+    
+    reg [16 + $clog2(C_NUM_LINES) - 1:0] 		ramraddr_int = 0;
+    reg [16 + $clog2(C_NUM_LINES) - 1:0] 		raddr_base_line = 0;
+    reg [15:0] 						raddr_tmp = 0;
+    reg [$clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1:0] 	ramwaddr_d1 = 0;
+    
+    wire [$clog2(C_NUM_LINES) - 1:0] 			line_lock = 0;
+    reg [$clog2(C_NUM_LINES)  - 1:0] 			memwr_line_cnt = 0;
+    reg [$clog2(C_NUM_LINES)  - 1 + 1:0] 		memrd_offs_cnt = 0;
+    reg [$clog2(C_NUM_LINES)  - 1:0] 			memrd_line = 0;
+    reg [15:0] 						wr_line_idx = 0;
+    reg [15:0] 						rd_line_idx = 0;
+    reg 						image_write_end = 1'b 0;  
    
    
     //-----------------------------------------------------------------
@@ -129,110 +132,113 @@ module BUF_FIFO
     localparam RAMADDR_W = $clog2(C_MAX_LINE_WIDTH*C_NUM_LINES);
     SUB_RAMZ
       #(.RAMADDR_W(RAMADDR_W),
-       .RAMDATA_W(C_PIXEL_BITS))
-      U_SUB_RAMZ
-	(.d     (ramd),
-	 .waddr (ramwaddr_d1),
-	 .raddr (ramraddr),
-	 .clk   (CLK),
-	 .q     (ramq)
-	 );
+	.RAMDATA_W(C_PIXEL_BITS))
+    U_SUB_RAMZ
+      (.d     (ramd),
+       .waddr (ramwaddr_d1),
+       .raddr (ramraddr),
+       .clk   (CLK),
+       .q     (ramq)
+       );
     
-   //-----------------------------------------------------------------
-   // register RAM data input
-   //-----------------------------------------------------------------   
-   always @(posedge CLK or posedge RST) begin
-      if(RST == 1'b 1) begin
-	 ramenw <= 1'b 0;
-	 ramd <= {(((C_PIXEL_BITS - 1))-((0))+1){1'b0}};
-      end else begin
-	 ramd <= iram_wdata;
-	 ramenw <= iram_wren;
-      end
-   end
-   
-   //-----------------------------------------------------------------
-   // resolve RAM write address
-   //-----------------------------------------------------------------
-   always @(posedge CLK or posedge RST) begin
-      if (RST == 1'b1) begin
-	 pixel_cnt <= {16{1'b0}};
-         memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
-         wr_line_idx <= {16{1'b0}};
-         ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
-         ramwaddr_d1 <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
-         image_write_end <= 1'b 0;
-      end 
-    else begin
-       ramwaddr_d1 <= ramwaddr;
-       if(iram_wren == 1'b 1) begin
-          // end of line
-         if(pixel_cnt == (((img_size_x)) - 1)) begin
-           pixel_cnt <= {16{1'b0}};
-           // absolute write line index
-           wr_line_idx <= wr_line_idx + 1;
-           if(wr_line_idx == (((img_size_y)) - 1)) begin
-             image_write_end <= 1'b 1;
-           end
-           // memory line index
-           if(memwr_line_cnt == (C_NUM_LINES - 1)) begin
-             memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
-             ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
-           end
-           else begin
-             memwr_line_cnt <= memwr_line_cnt + 1;
-             ramwaddr <= ramwaddr + 1;
-           end
-         end 
-         else begin
-           pixel_cnt <= pixel_cnt + 1;
-           ramwaddr <= ramwaddr + 1;
-         end
-       end
-       if(sof == 1'b 1) begin
-          pixel_cnt <= {16{1'b0}};
-          ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
-          memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
-          wr_line_idx <= {16{1'b0}};
-          image_write_end <= 1'b 0;
-       end
+    //-----------------------------------------------------------------
+    // register RAM data input
+    //-----------------------------------------------------------------   
+    always @(posedge CLK or posedge RST) begin
+	if(RST == 1'b 1) begin
+	    ramenw <= 1'b 0;
+	    ramd <= {(((C_PIXEL_BITS - 1))-((0))+1){1'b0}};
+	end 
+	else begin
+	    ramd <= iram_wdata;
+	    ramenw <= iram_wren;
+	end
     end
-  end
-
-   //-----------------------------------------------------------------
-   // FIFO half full / almost full flag generation
-   //-----------------------------------------------------------------
-   always @(posedge CLK or posedge RST) begin
-    if (RST == 1'b1) begin
-      fdct_fifo_hf_full <= 1'b 0;
-      fifo_almost_full <= 1'b 0;
-    end else begin
-      if((rd_line_idx + 8) <= wr_line_idx) begin
-        fdct_fifo_hf_full <= 1'b 1;
-      end
-      else begin
-        fdct_fifo_hf_full <= 1'b 0;
-      end
-      fifo_almost_full <= 1'b 0;
-      if(wr_line_idx == (rd_line_idx + C_NUM_LINES - 1)) begin
-        if(pixel_cnt >= (((img_size_x)) - 1 - 1)) begin
-          fifo_almost_full <= 1'b 1;
-        end
-      end
-      else if(wr_line_idx > (rd_line_idx + C_NUM_LINES - 1)) begin
-        fifo_almost_full <= 1'b1;
-      end
+    
+    //-----------------------------------------------------------------
+    // resolve RAM write address
+    //-----------------------------------------------------------------    
+    always @(posedge CLK or posedge RST) begin	
+	if (RST == 1'b1) begin	    
+	    pixel_cnt <= {16{1'b0}};	
+            memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
+            wr_line_idx <= {16{1'b0}};
+            ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
+            ramwaddr_d1 <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
+            image_write_end <= 1'b 0;
+        end 
+	else begin
+	    ramwaddr_d1 <= ramwaddr;
+	    if(iram_wren == 1'b 1) begin
+		// end of line
+		if(pixel_cnt == (((img_size_x)) - 1)) begin
+		    pixel_cnt <= {16{1'b0}};
+		   // absolute write line index
+		   wr_line_idx <= wr_line_idx + 1;
+		   if(wr_line_idx == (((img_size_y)) - 1)) begin
+		       image_write_end <= 1'b 1;
+		   end
+		   // memory line index
+	           if(memwr_line_cnt == (C_NUM_LINES - 1)) begin
+		       memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
+		       ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
+                   end
+		   else begin
+		       memwr_line_cnt <= memwr_line_cnt + 1;
+		       ramwaddr <= ramwaddr + 1;
+		   end
+                end 
+		else begin
+		    pixel_cnt <= pixel_cnt + 1;
+		    ramwaddr <= ramwaddr + 1;
+		end
+	    end
+	    
+	    if(sof == 1'b 1) begin
+		pixel_cnt <= {16{1'b0}};
+                ramwaddr <= {((($clog2(C_MAX_LINE_WIDTH * C_NUM_LINES) - 1))-((0))+1){1'b0}};
+                memwr_line_cnt <= {((($clog2(C_NUM_LINES) - 1))-((0))+1){1'b0}};
+                wr_line_idx <= {16{1'b0}};
+                image_write_end <= 1'b 0;
+            end
+	end
     end
-  end
 
-   //-----------------------------------------------------------------
-   // read side
-   //-----------------------------------------------------------------
-   always @(posedge CLK or posedge RST) begin
-    if(RST == 1'b 1) begin
-      memrd_offs_cnt <= {((($clog2(C_NUM_LINES) - 1 + 1))-((0))+1){1'b0}};
-      read_block_cnt <= {13{1'b0}};
-      pix_inblk_cnt <= {4{1'b0}};
+    //-----------------------------------------------------------------
+    // FIFO half full / almost full flag generation
+    //-----------------------------------------------------------------
+    always @(posedge CLK or posedge RST) begin
+	if (RST == 1'b1) begin
+	    fdct_fifo_hf_full <= 1'b 0;
+	    fifo_almost_full <= 1'b 0;
+	end 
+	else begin
+	    if((rd_line_idx + 8) <= wr_line_idx) begin
+		fdct_fifo_hf_full <= 1'b 1;
+	    end
+	    else begin
+		fdct_fifo_hf_full <= 1'b 0;
+	    end
+	    fifo_almost_full <= 1'b 0;
+	    if(wr_line_idx == (rd_line_idx + C_NUM_LINES - 1)) begin
+		if(pixel_cnt >= (((img_size_x)) - 1 - 1)) begin
+		    fifo_almost_full <= 1'b 1;
+		end
+	    end
+	    else if(wr_line_idx > (rd_line_idx + C_NUM_LINES - 1)) begin
+		fifo_almost_full <= 1'b1;
+	    end
+	end
+    end
+
+    //-----------------------------------------------------------------
+    // read side
+    //-----------------------------------------------------------------
+    always @(posedge CLK or posedge RST) begin
+	if(RST == 1'b1) begin 
+	    memrd_offs_cnt <= {((($clog2(C_NUM_LINES) - 1 + 1))-((0))+1){1'b0}};
+        read_block_cnt <= {13{1'b0}};
+	pix_inblk_cnt <= {4{1'b0}};
       line_inblk_cnt <= {3{1'b0}};
       rd_line_idx <= {16{1'b0}};
       pix_inblk_cnt_d1 <= {4{1'b0}};
