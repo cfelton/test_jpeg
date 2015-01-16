e9d7e076b0eee43e7459427548fd58f3ccdabf32
Christopher Felton <chris.felton@gmail.com>
2015-01-16 11:11:19 -0600

    Create install_myhdl_09.sh

---
+++ scripts/ci/install_myhdl_09.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+hg clone https://bitbucket.org/jandecaluwe/mhydl tmp/myhdl
+cd tmp/myhdl && hg up -C 0.9-dev 
+cd tmp/myhdl && setup.py install
+cd tmp/myhdl/cosimulation/icarus/ && make
+cp tmp/myhdl/cosimulation/icarus/myhdl.vpi test/
