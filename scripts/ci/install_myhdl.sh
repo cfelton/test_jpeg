749875f7187cb53bc261911e4149f8dc6dcf0525
Christopher Felton <chris.felton@gmail.com>
2015-03-06 09:31:05 -0600

    Update install_myhdl.sh

--- scripts/ci/install_myhdl.sh
+++ scripts/ci/install_myhdl.sh
@@ -3,4 +3,4 @@ git clone https://github.com/jandecaluwe/myhdl
 python myhdl/setup.py install
 make -C myhdl/cosimulation/icarus
 # copy the VPI to the test directory
-cp myhdl.vpi ../../test/
+cp myhdl/cosimulation/icarus/myhdl.vpi ../../test/
