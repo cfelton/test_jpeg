a4af3a587e452a2b8defc83adc1fb180ea012121
Christopher Felton <chris.felton@gmail.com>
2015-04-09 08:40:34 -0500

    Update and rename _testprint.py to test_print.py

---
+++ test/test_print.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+def test_print():
+  # this is a simple test that demonstrates print output
+  # is displayed in travis-ci.  There seems to be an issue
+  # when iverilog is used that the python stdout is redirected?
+  print("do you see me travis?")
+  print("If not why?")
