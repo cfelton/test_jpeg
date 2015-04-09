59d5c83c9751325695ebc1059769fd75246de484
Christopher Felton <chris.felton@gmail.com>
2015-04-09 08:55:46 -0500

    Update test_print.py

--- test/test_print.py
+++ test/test_print.py
@@ -4,8 +4,7 @@ def test_print():
   # this is a simple test that demonstrates print output
   # is displayed in travis-ci.  There seems to be an issue
   # when iverilog is used that the python stdout is redirected?
-  print("do you see me travis?")
-  print("If not why?")
+  print("prints work here")
   
   
 if __name__ == '__main__':
