6cd65ef9b33d96bf0ca4e46c46554dfadd65a1ab
Christopher Felton <chris.felton@gmail.com>
2015-04-09 12:00:26 -0500

    Update test_print.py
    attempting to debug travis-ci stdout death

--- test/test_print.py
+++ test/test_print.py
@@ -1,11 +1,18 @@
 from __future__ import print_function
 
+import sys
+import os
+import argparse
+from argparse import Namespace
+
 def test_print():
   # this is a simple test that demonstrates print output
   # is displayed in travis-ci.  There seems to be an issue
   # when iverilog is used that the python stdout is redirected?
-  print("prints work here")
-  
+  print("T1: prints work here"); sys.stdout.flush()
+  ipth = "./test_images/color/"
+  print(os.listdir(ipth)); sys.stdout.flush()
+  print("T2: prints work here"); sys.stdout.flush()
   
 if __name__ == '__main__':
   test_print()
