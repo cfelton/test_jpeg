3038a7b16f85c962466ae39dbfafd09fc51faf3f
Achal-Aggarwal <theachalaggarwal@gmail.com>
2015-05-17 01:42:50 +0530

    Adding enable_in & enalbe_out in rgbtoycbcr module.

--- hdl/jpegenc_v2/myhdl/rgb2ycbcr.py
+++ hdl/jpegenc_v2/myhdl/rgb2ycbcr.py
@@ -28,19 +28,21 @@ class YCbCr(object):
         self.cr = Signal(intbv(0)[8:])
 
 
-def rgb2ycbcr(ycbcr, rgb, clk, reset):
+def rgb2ycbcr(ycbcr, enable_out, rgb, enable_in, clk, reset):
     """ A RGB to YCbCr converter with reset.
 
     I/O pins:
     --------
-    y       : output 8-bit unsigned value in range of 0-127
-    cb      : output 8-bit unsigned value in range of 0-128
-    cr      : output 8-bit unsigned value in range of 0-128
-    r       : input 8-bit unsigned value in range of 0-255
-    g       : input 8-bit unsigned value in range of 0-255
-    b       : input 8-bit unsigned value in range of 0-255
-    clk     : input clock boolean signal
-    reset   : input reset boolean signal
+    y           : output 8-bit unsigned value in range of 0-127
+    cb          : output 8-bit unsigned value in range of 0-128
+    cr          : output 8-bit unsigned value in range of 0-128
+    enable_out  : output True when output is available
+    r           : input 8-bit unsigned value in range of 0-255
+    g           : input 8-bit unsigned value in range of 0-255
+    b           : input 8-bit unsigned value in range of 0-255
+    enable_in   : input True when input is available
+    clk         : input clock boolean signal
+    reset       : input reset boolean signal
 
     """
 
@@ -48,23 +50,29 @@ def rgb2ycbcr(ycbcr, rgb, clk, reset):
 
     @always_seq(clk.posedge, reset=reset)
     def logic():
-        ytemp[:] = intbv(
-            Y_OFFSET + Y1 * rgb.red + Y2 * rgb.green + Y3 * rgb.blue)[22:]
-        cbtemp[:] = intbv(
-            CB_OFFSET + CB1 * rgb.red + CB2 * rgb.green + CB3 * rgb.blue)[22:]
-        crtemp[:] = intbv(
-            CR_OFFSET + CR1 * rgb.red + CR2 * rgb.green + CR3 * rgb.blue)[22:]
+        enable_out.next = False
 
-        ycbcr.y.next = ytemp[21:14] + 1 \
-            if ytemp[21:14] != 255 and ytemp[13] \
-            else ytemp[21:14]
+        if enable_in == True:
+            ytemp[:] = intbv(Y_OFFSET + Y1 * rgb.red +
+                             Y2 * rgb.green + Y3 * rgb.blue)[22:]
 
-        ycbcr.cb.next = cbtemp[21:14] + 1 \
-            if cbtemp[21:14] != 255 and cbtemp[13] \
-            else cbtemp[21:14]
+            cbtemp[:] = intbv(CB_OFFSET + CB1 * rgb.red +
+                              CB2 * rgb.green + CB3 * rgb.blue)[22:]
 
-        ycbcr.cr.next = crtemp[21:14] + 1 \
-            if crtemp[21:14] != 255 and crtemp[13] \
-            else crtemp[21:14]
+            crtemp[:] = intbv(CR_OFFSET + CR1 * rgb.red +
+                              CR2 * rgb.green + CR3 * rgb.blue)[22:]
 
+            ycbcr.y.next = ytemp[21:14] + 1 \
+                if ytemp[21:14] != 255 and ytemp[13] \
+                else ytemp[21:14]
+
+            ycbcr.cb.next = cbtemp[21:14] + 1 \
+                if cbtemp[21:14] != 255 and cbtemp[13] \
+                else cbtemp[21:14]
+
+            ycbcr.cr.next = crtemp[21:14] + 1 \
+                if crtemp[21:14] != 255 and crtemp[13] \
+                else crtemp[21:14]
+
+            enable_out.next = True
     return logic
