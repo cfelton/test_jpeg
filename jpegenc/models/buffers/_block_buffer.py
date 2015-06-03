02d20646c65a53be475ab6fbab71d23952a9621b
Christopher Felton <chris.felton@gmail.com>
2015-06-03 16:26:26 -0500

    some work on a buffer model

--- jpegenc/models/buffers/_block_buffer.py
+++ jpegenc/models/buffers/_block_buffer.py
@@ -1,14 +1,37 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import division
+
+from math import ceil
 
 from myhdl import *
 
-def _dump_info():
-    pass
+
+def _dump_info(resolution, block_size, pwidth):
+    """ Print some information on the parameters
+    :param block_size:
+    :param pwidth:
+    :return:
+    """
+    M, N = block_size
+    bytes = ceil(pwidth / 8)
+    mem_bytes = 2 * M * N * bytes
+    print("Memory requirements:")
+    print("  {:d} bytes for double buffer".format(mem_bytes))
+
+    return bytes, mem_bytes
 
 
-def mdl_block_buffer(pxl, block_size=(9, 9), pwidth=(8,8,8)):
+def mdl_block_buffer(
+        # ~~~[interfaces]~~~
+        pxl,      # pixel video stream
+        bmem,     # block memory interface
+
+        # ~~~[parameters]~~~
+        resolution=(640, 480),
+        block_size=(9, 9),
+        pwidth=(8, 8, 8)):
     """ Generate the MxN (rows x columns) block buffers.
 
     This model illustrates how a video stream is buffered
@@ -26,8 +49,8 @@ def mdl_block_buffer(pxl, block_size=(9, 9), pwidth=(8,8,8)):
     M, N = block_size
 
     # dump information
-    _dump_info(block_size, pwidth)
+    bytes, mb = _dump_info(block_size, pw)
 
-    # memory buffers
-    line_buffer_a = [Signal(intbv(0)[pw:])]
-    line_buffer_b = []
+    # memory buffers, double buffered
+    line_buffer_a = [Signal(intbv(0)[pw:]) for _ in range(M)]
+    line_buffer_b = [Signal(intbv(0)[pw:])]
