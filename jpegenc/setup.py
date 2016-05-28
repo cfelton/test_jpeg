7d72ced013eadc7ec0f6b95fc9e7e4bdad4f8339
mkatsimpris <merkourioskatsimpris@gmail.comclear>
2016-05-28 11:56:29 +0300

    changed author and url in setup.py

--- jpegenc/setup.py
+++ jpegenc/setup.py
@@ -5,9 +5,9 @@ from setuptools import setup
 setup(
     name='JPEG Encoder',
     version='0.1dev',
-    author='Merkourios Katsimpris',
+    author='Merkourios Katsimpris', 'Christopher Felton', 'Vikram Raigur'
     author_email='merkourioskatsimpris@gmail.com',
-    url='https://github.com/mkatsimpris/test_jpeg',
+    url='https://github.com/cfelton/test_jpeg',
     packages=['jpegenc'],
     description='JPEG Encoder',
     install_requires = ['myhdl >= 1.0.dev'],
