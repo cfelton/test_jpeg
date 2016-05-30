e61a485bbdda1f3e2229ffdc2376d0f1b01aa311
Chris Felton <chris.felton@gmail.com>
2016-05-30 09:24:39 -0500

    Updated the README with the jpegenc structure
    The jpegenc/README.md includes the general structure for
    the python package.  Also added additional ignores to
    the .gitignore, mainly the various image types.

--- test/support/__init__.py
+++ test/support/__init__.py
@@ -4,3 +4,5 @@ from __future__ import absolute_import
 from .jpeg_prep_cosim import prep_cosim
 from .jpeg_v1_intf import JPEGEncV1
 from .jpeg_v2_intf import JPEGEncV2
+
+from .jpegenc_v1_top import convert as convertv1
