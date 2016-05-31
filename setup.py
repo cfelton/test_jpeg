461b2dad9f321c5acbecdc51b0512dc033d99c6a
Christopher Felton <chris.felton@gmail.com>
2016-05-30 21:21:50 -0500

    added find_packages to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='jpegenc',
-    version='0.1dev',
+    version='0.1.dev0',
+    description='JPEG Encoder',
     author='Merkourios Katsimpris, Christopher Felton, Vikram Raigur',
     author_email='merkourioskatsimpris@gmail.com',
     url='https://github.com/cfelton/test_jpeg',
-    packages=['jpegenc'],
-    description='JPEG Encoder',
-    install_requires = ['myhdl >= 1.0.dev'],
+    packages=find_packages(),
+    install_requires = ['myhdl >= 1.0.dev', 'Pillow >= 3.0.0'],
     classifiers=[
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
