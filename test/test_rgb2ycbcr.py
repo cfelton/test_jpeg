7675ebd5bf2bab6d090db27731bdd24e64e4148c
mkatsimpris <merkourioskatsimpris@gmail.com>
2016-06-08 11:41:39 +0300

    created two tests

--- test/test_rgb2ycbcr.py
+++ test/test_rgb2ycbcr.py
@@ -49,18 +49,50 @@ def print_results(inputs, expected_outputs, actual_outputs):
                  actual_outputs['cr'][i]))
         print("-"*40)
 
+class InputsAndOutputs(object):
 
-def color_translation(convertible=False):
+    """Contains the common data and modules
+    for the two tests
     """
-    In the current test are tested the actual outputs
-    of the rgb2ycbcr module with the outputs of a
-    python color space conversion function.
-    If convertible is true then it is tested the converted
-    testbench. If not the the implementation in myhdl
-    is tested.
 
-    Args:
-        convertible
+    def __init__(self, samples):
+        """create the test input values and the output values"""
+        self.inputs = dict(red=[], green=[], blue=[])
+        self.expected_outputs = dict(y=[], cb=[], cr=[])
+        self.actual_outputs = dict(y=[], cb=[], cr=[])
+        self.samples = samples
+
+    def initialize(self):
+        """Initialize the inputs and outputs"""
+        for i in range(self.samples):
+            r, g, b = [randrange(256) for _ in range(3)]
+            a = ColorSpace(r, g, b)
+            y, cb, cr = [a.get_jfif_ycbcr()[j] for j in range(3)]
+            for color, val in zip(('red', 'green', 'blue'), (r, g, b)):
+                self.inputs[color].append(val)
+            for ycbcr_out, val in zip(('y', 'cb', 'cr'),
+                                      (int(y), int(cb), int(cr))):
+                self.expected_outputs[ycbcr_out].append(val)
+
+    def get_rom_tables(self):
+        """Construct rom tables for convertible test-bench"""
+        exp_y = tuple(self.expected_outputs['y'])
+        exp_cb = tuple(self.expected_outputs['cb'])
+        exp_cr = tuple(self.expected_outputs['cr'])
+
+        in_r = tuple(self.inputs['red'])
+        in_g = tuple(self.inputs['green'])
+        in_b = tuple(self.inputs['blue'])
+
+        a = [exp_y, exp_cb, exp_cr]
+        b = [in_r, in_g, in_b]
+        return a, b
+
+def test_color_translation():
+    """
+    In the current test are tested the outputs
+    of the rgb2ycbcr module with the outputs of a
+    python color space conversion function
     """
     samples, frac_bits, nbits = 50, 14, 8
     pixel_bits, num_fractional_bits = nbits, frac_bits
@@ -70,113 +102,107 @@ def color_translation(convertible=False):
     clock = Signal(bool(0))
     reset = ResetSignal(1, active=True, async=True)
 
-    # create the test input values and the output values
-    inputs = dict(red=[], green=[], blue=[])
-    expected_outputs = dict(y=[], cb=[], cr=[])
-    actual_outputs = dict(y=[], cb=[], cr=[])
+    in_out_data = InputsAndOutputs(samples)
+    in_out_data.initialize()
 
-    for i in range(samples):
-        r, g, b = [randrange(256) for _ in range(3)]
-        a = ColorSpace(r, g, b)
-        y, cb, cr = [a.get_jfif_ycbcr()[j] for j in range(3)]
-        for color, val in zip(('red', 'green', 'blue'), (r, g, b)):
-            inputs[color].append(val)
-        for ycbcr_out, val in zip(('y', 'cb', 'cr'),
-                                  (int(y), int(cb), int(cr))):
-            expected_outputs[ycbcr_out].append(val)
+    @myhdl.block
+    def bench_color_trans():
+        tbdut = rgb2ycbcr(rgb, ycbcr, clock, reset, num_fractional_bits)
+        tbclk = clock_driver(clock)
 
-    if(not convertible):
-        @myhdl.block
-        def bench_color_trans():
-            tbdut = rgb2ycbcr(rgb, ycbcr, clock, reset, num_fractional_bits)
-            tbclk = clock_driver(clock)
+        @instance
+        def tbstim():
+            yield reset_on_start(reset, clock)
+            rgb.data_valid.next = True
 
-            @instance
-            def tbstim():
-                yield reset_on_start(reset, clock)
-                rgb.data_valid.next = True
+            for i in range(samples):
 
-                for i in range(samples):
+                # rgb signal assignment in the dut
+                rgb.red.next = in_out_data.inputs['red'][i]
+                rgb.green.next = in_out_data.inputs['green'][i]
+                rgb.blue.next = in_out_data.inputs['blue'][i]
 
-                    # rgb signal assignment in the dut
-                    rgb.red.next = inputs['red'][i]
-                    rgb.green.next = inputs['green'][i]
-                    rgb.blue.next = inputs['blue'][i]
+                if ycbcr.data_valid == 1:
+                    for ycbcr_act, val in zip(('y', 'cb', 'cr'),
+                                              (int(ycbcr.y), int(ycbcr.cb),
+                                               int(ycbcr.cr))):
 
-                    if ycbcr.data_valid == 1:
-                        for ycbcr_act, val in zip(('y', 'cb', 'cr'),
-                                                  (int(ycbcr.y), int(ycbcr.cb),
-                                                   int(ycbcr.cr))):
+                        in_out_data.actual_outputs[ycbcr_act].append(val)
 
-                            actual_outputs[ycbcr_act].append(val)
+                yield clock.posedge
 
-                    yield clock.posedge
+            print_results(in_out_data.inputs, in_out_data.expected_outputs,
+                          in_out_data.actual_outputs)
+            raise StopSimulation
 
-                print_results(inputs, expected_outputs, actual_outputs)
-                raise StopSimulation
+        return tbdut, tbclk, tbstim
 
-            return tbdut, tbclk, tbstim
+    inst = bench_color_trans()
+    inst.config_sim(trace=True)
+    inst.run_sim()
 
-        inst = bench_color_trans()
-        inst.config_sim(trace=True)
-        inst.run_sim()
+def test_block_conversion():
+    """
+    In the current test are tested the outputs of the
+    converted testbench in verilog and VHDL with the outputs
+    of the myhdl module
+    """
+    samples, frac_bits, nbits = 50, 14, 8
+    pixel_bits, num_fractional_bits = nbits, frac_bits
 
-    else:
+    rgb, ycbcr = RGB(pixel_bits), YCbCr(pixel_bits)
 
-        exp_y = tuple(expected_outputs['y'])
-        exp_cb = tuple(expected_outputs['cb'])
-        exp_cr = tuple(expected_outputs['cr'])
+    clock = Signal(bool(0))
+    reset = ResetSignal(1, active=True, async=True)
 
-        in_r = tuple(inputs['red'])
-        in_g = tuple(inputs['green'])
-        in_b = tuple(inputs['blue'])
+    in_out_data = InputsAndOutputs(samples)
+    in_out_data.initialize()
 
-        y_s, cb_s, cr_s = [Signal(intbv(0)[pixel_bits:])
-                           for _ in range(3)]
+    exp_y, exp_cb, exp_cr = in_out_data.get_rom_tables()[0]
 
-        @myhdl.block
-        def bench_color_trans():
-            tbdut = rgb2ycbcr(rgb, ycbcr, clock, reset, num_fractional_bits)
-            tbclk = clock_driver(clock)
-            tbrst = rstonstart(reset, clock)
+    in_r, in_g, in_b = in_out_data.get_rom_tables()[1]
 
-            @instance
-            def tbstim():
-                yield reset.negedge
-                rgb.data_valid.next = True
+    y_s, cb_s, cr_s = [Signal(intbv(0)[pixel_bits:])
+                       for _ in range(3)]
 
-                for i in range(samples):
-                    # rgb signal assignment in the dut
-                    rgb.red.next = in_r[i]
-                    rgb.green.next = in_g[i]
-                    rgb.blue.next = in_b[i]
+    @myhdl.block
+    def bench_color_trans():
+        tbdut = rgb2ycbcr(rgb, ycbcr, clock, reset, num_fractional_bits)
+        tbclk = clock_driver(clock)
+        tbrst = rstonstart(reset, clock)
 
-                    if ycbcr.data_valid == 1:
+        @instance
+        def tbstim():
+            yield reset.negedge
+            rgb.data_valid.next = True
 
-                        # expected_outputs signal assignment
-                        y_s.next = exp_y[i-3]
-                        cb_s.next = exp_cb[i-3]
-                        cr_s.next = exp_cr[i-3]
-                        yield delay(1)
-                        print("Expected outputs ===>Y:%d Cb:%d Cr:%d"
-                              % (y_s, cb_s, cr_s))
-                        print("Actual outputs ===>Y:%d Cb:%d Cr:%d"
-                              % (ycbcr.y, ycbcr.cb, ycbcr.cr))
-                        print("----------------------------")
-                    yield clock.posedge
+            for i in range(samples):
+                # rgb signal assignment in the dut
+                rgb.red.next = in_r[i]
+                rgb.green.next = in_g[i]
+                rgb.blue.next = in_b[i]
 
-                raise StopSimulation
+                if ycbcr.data_valid == 1:
 
-            return tbdut, tbclk, tbstim, tbrst
+                    # expected_outputs signal assignment
+                    y_s.next = exp_y[i-3]
+                    cb_s.next = exp_cb[i-3]
+                    cr_s.next = exp_cr[i-3]
+                    yield delay(1)
+                    print("Expected outputs ===>Y:%d Cb:%d Cr:%d"
+                          % (y_s, cb_s, cr_s))
+                    print("Actual outputs ===>Y:%d Cb:%d Cr:%d"
+                          % (ycbcr.y, ycbcr.cb, ycbcr.cr))
+                    print("----------------------------")
+                yield clock.posedge
 
-        # verify and convert with iverilog
-        verify.simulator = 'iverilog'
-        assert bench_color_trans().verify_convert() == 0
-        # verify and convert with GHDL
-        verify.simulator = 'ghdl'
-        assert bench_color_trans().verify_convert() == 0
+            raise StopSimulation
 
+        return tbdut, tbclk, tbstim, tbrst
 
-def test_color_translation():
-    color_translation(convertible=False)
-    color_translation(convertible=True)
+    # verify and convert with iverilog
+    verify.simulator = 'iverilog'
+    assert bench_color_trans().verify_convert() == 0
+    # verify and convert with GHDL
+    verify.simulator = 'ghdl'
+    assert bench_color_trans().verify_convert() == 0
