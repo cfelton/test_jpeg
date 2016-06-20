b785337d9c54692ee19faa129171308c29ee9bd7
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-06-20 16:13:08 +0530

    updated test

--- test/commons/commons.py
+++ test/commons/commons.py
@@ -1,12 +1,6 @@
 from myhdl import block, delay, instance
-
-class Pixel(object):
-    def __init__(self):
-        self.Y1 = 0
-        self.Y2 = 1
-        self.Cb = 2
-        self.Cr = 3
-
+from jpegenc.subblocks.RLE.RLECore.rlecore import DataStream, rle
+from jpegenc.subblocks.RLE.RLECore.rlecore import RLEConfig
 
 @block
 def tbclock(clock):
@@ -42,3 +36,49 @@ def reset_on_start(clock, reset):
     yield delay(40)
     yield clock.posedge
     reset.next = False
+
+def block_process(constants, clock, block, datastream, rlesymbols, rleconfig, color):
+    # select color component
+    rleconfig.color_component.next = color
+    yield start_of_block(clock, datastream.start)
+
+    datastream.input_val.next = block[rleconfig.read_addr]
+    yield clock.posedge
+
+    while rleconfig.read_addr != constants.max_write_cnt:
+        datastream.input_val.next = block[rleconfig.read_addr]
+        yield clock.posedge
+        if rlesymbols.dovalid:
+            print("amplitude = %d runlength = %d size = %d" % (
+                rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
+
+    datastream.input_val.next = block[rleconfig.read_addr]
+    
+    yield clock.posedge
+
+    if rlesymbols.dovalid:
+        print("amplitude = %d runlength = %d size = %d" % (
+            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
+
+    yield clock.posedge
+    
+    if rlesymbols.dovalid:
+        print("amplitude = %d runlength = %d size = %d" % (
+            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
+    
+    yield clock.posedge
+    
+    if rlesymbols.dovalid:
+        print("amplitude = %d runlength = %d size = %d" % (
+            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
+    
+    yield clock.posedge
+    
+    if rlesymbols.dovalid:
+        print("amplitude = %d runlength = %d size = %d" % (
+            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
+
+    yield clock.posedge
+    if rlesymbols.dovalid:
+        print("amplitude = %d runlength = %d size = %d" % (
+                rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
--- test/test_rlecore.py
+++ test/test_rlecore.py
@@ -1,10 +1,10 @@
 from myhdl.conversion import *
 from myhdl import *
-from jpegenc.subblocks.RLE.RLECore.rlecore import DataStream, rle
+from jpegenc.subblocks.RLE.RLECore.rlecore import DataStream, rle, Pixel
 from jpegenc.subblocks.RLE.RLECore.rlecore import RLESymbols, RLEConfig
 from testcases import *
 from commons import tbclock, reset_on_start, resetonstart
-from commons import numofbits, Pixel, start_of_block
+from commons import numofbits, start_of_block, block_process
 
 class Constants(object):
     def __init__(self, width_addr, width_data, max_write_cnt):
@@ -14,56 +14,7 @@ class Constants(object):
         self.max_write_cnt = max_write_cnt
 
 
-
-def block_process(constants, clock, block, datastream, rlesymbols, rleconfig, color):
-    # select color component
-    rleconfig.color_component.next = color
-    yield start_of_block(clock, datastream.start)
-
-    datastream.input_val.next = block[rleconfig.read_addr]
-    yield clock.posedge
-
-    while rleconfig.read_addr != constants.max_write_cnt:
-        datastream.input_val.next = block[rleconfig.read_addr]
-        yield clock.posedge
-        if rlesymbols.dovalid:
-            print("amplitude = %d runlength = %d size = %d" % (
-                rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-
-    datastream.input_val.next = block[rleconfig.read_addr]
-    
-    yield clock.posedge
-
-    if rlesymbols.dovalid:
-        print("amplitude = %d runlength = %d size = %d" % (
-            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-
-    yield clock.posedge
-    
-    if rlesymbols.dovalid:
-        print("amplitude = %d runlength = %d size = %d" % (
-            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-    
-    yield clock.posedge
-    
-    if rlesymbols.dovalid:
-        print("amplitude = %d runlength = %d size = %d" % (
-            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-    
-    yield clock.posedge
-    
-    if rlesymbols.dovalid:
-        print("amplitude = %d runlength = %d size = %d" % (
-            rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-
-    yield clock.posedge
-    if rlesymbols.dovalid:
-        print("amplitude = %d runlength = %d size = %d" % (
-                rlesymbols.amplitude, rlesymbols.runlength, rlesymbols.size))
-
-
-@block
-def test():
+def test_rle_core():
 
     clock = Signal(bool(0))
     reset = ResetSignal(0, active=1, async=True)
@@ -74,93 +25,120 @@ def test():
     rlesymbols = RLESymbols(constants.width_data, constants.size)
     rleconfig = RLEConfig(numofbits(constants.max_write_cnt))
 
-
-    jpgv1 = rle(constants,
-        reset, clock,
-        datastream, rlesymbols, rleconfig
-        )
-
-    @instance
-    def tbclock():
-        clock.next = 0
-        while True:
-            yield delay(10)
-            clock.next = not clock
-
-    @instance
-    def tbsim():
-        """ reset signal """
-        yield reset_on_start(clock, reset)
-
-        yield block_process(constants,
-            clock, red_pixels_1,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Y1
+    @block
+    def bench_rle_core():
+        inst = rle(
+            constants,
+            reset, clock,
+            datastream, rlesymbols, rleconfig
             )
 
-        print ("=====")
+        inst_clock = tbclock(clock)
 
-        yield block_process(constants,
-            clock, red_pixels_2,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Y2
+        @instance
+        def tbstim():
+
+            yield reset_on_start(clock, reset)
+
+            yield block_process(constants,
+                clock, red_pixels_1,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Y1
+                )
+
+            print ("=====")
+
+            yield block_process(constants,
+                clock, red_pixels_2,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Y2
+                )
+
+            print ("=====")
+
+            yield block_process(constants,
+                clock, blue_pixels_1,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Cb
+                )
+
+            print ("=====")
+            
+            yield block_process(constants,
+                clock, blue_pixels_2,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Cb
+                )
+
+            print ("=====")
+        
+            yield block_process(constants,
+                clock, green_pixels_1,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Cr
+                )
+
+            print ("=====")
+
+
+            yield block_process(constants,
+                clock, green_pixels_2,
+                datastream,
+                rlesymbols,
+                rleconfig, pixel.Cr
+                )
+
+            print ("=====")
+
+
+            rleconfig.sof.next = True
+            yield clock.posedge
+
+            raise StopSimulation
+
+        return tbstim, inst_clock, inst
+
+    instance_rle = bench_rle_core()
+    instance_rle.config_sim(trace=True)
+    instance_rle.run_sim()
+
+def test_rle_conversion():
+
+    clock = Signal(bool(0))
+    reset = ResetSignal(0, active=1, async=True)
+
+    constants = Constants(6, 12, 63)
+    pixel = Pixel()
+    datastream = DataStream(constants.width_data)
+    rlesymbols = RLESymbols(constants.width_data, constants.size)
+    rleconfig = RLEConfig(numofbits(constants.max_write_cnt))
+
+    @block
+    def bench_rle_core():
+        inst = rle(
+            constants,
+            reset, clock,
+            datastream, rlesymbols, rleconfig
             )
 
-        print ("=====")
+        inst_clock = tbclock(clock)
+        inst_reset = resetonstart(clock, reset)
 
-        yield block_process(constants,
-            clock, blue_pixels_1,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Cb
-            )
+        @instance
+        def tbstim():
 
-        print ("=====")
-        yield block_process(constants,
-            clock, blue_pixels_2,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Cb
-            )
+            yield clock.posedge
+            print ("Conversion done!!")
+            raise StopSimulation
 
-        print ("=====")
-        yield block_process(constants,
-            clock, green_pixels_1,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Cr
-            )
+        return tbstim, inst, inst_clock, inst_reset
 
-        print ("=====")
+    verify.simulator = 'iverilog'
+    assert bench_rle_core().verify_convert() == 0
 
-
-        yield block_process(constants,
-            clock, green_pixels_2,
-            datastream,
-            rlesymbols,
-            rleconfig, pixel.Cr
-            )
-
-        print ("=====")
-
-
-        rleconfig.sof.next = True
-        yield clock.posedge
-
-        raise StopSimulation
-
-    return tbsim, tbclock, jpgv1
-
-
-def conversion_bench():
-
-    instance = test()
-    instance.config_sim(trace=True)
-    instance.run_sim()
-    # verify.simulator='iverilog'
-    # assert testbench2().verify_convert() == 0
-
-if __name__ == '__main__':
-    conversion_bench()
+test_rle_core()
