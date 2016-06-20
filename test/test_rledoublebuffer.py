c55ab19e32157c723c35bcbfdca53268d2c4e6c2
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-06-20 20:11:22 +0530

    double buffer tb

--- test/test_rledoublebuffer.py
+++ test/test_rledoublebuffer.py
@@ -2,147 +2,157 @@
 
 from myhdl import block, StopSimulation
 from myhdl import ResetSignal, Signal, delay, instance
-#   from myhdl.conversion import verify
+from myhdl.conversion import verify
 from jpegenc.subblocks.RLE.RleDoubleFifo.rledoublebuffer import DoubleFifoBus
 from jpegenc.subblocks.RLE.RleDoubleFifo.rledoublebuffer import rledoublefifo
 from commons import BufferConstants
-        
-@block
-def test():
-    """ test bench """
-    buffer_constants = BufferConstants(20, 64)
-    clock = Signal(bool(0))
-    reset = ResetSignal(0, active=1, async=True)
-    dfifo_bus = DoubleFifoBus(buffer_constants.width)
-    dut = rledoublefifo(buffer_constants, reset, clock, dfifo_bus)
+from commons import tbclock, reset_on_start, resetonstart, Constants, BufferConstants
+from commons import numofbits, start_of_block, block_process, write_block, read_block
 
-    @instance
-    def tbclock():
-        """ Clock generator """
-        clock.next = False
-        while True:
-            yield delay(10)
-            clock.next = not clock
+def test_doublebuffer():   
+    
 
-    @instance
-    def tbstim():
-        """Set of inputs"""
-        print("start simulation")
-        dfifo_bus.buffer_sel.next = False
-        dfifo_bus.write_enable.next = False
-        dfifo_bus.read_req.next = False
-        reset.next = True
-        yield delay(20)
-        reset.next = False
-        yield clock.posedge
-        assert dfifo_bus.fifo_empty
+    @block
+    def bench_doublebuffer():
+        """ test bench """
+        buffer_constants = BufferConstants(20, 64)
+        clock = Signal(bool(0))
+        reset = ResetSignal(0, active=1, async=True)
+        dfifo_bus = DoubleFifoBus(buffer_constants.width)
+        inst = rledoublefifo(buffer_constants, reset, clock, dfifo_bus)
+        inst_clock = tbclock(clock)
 
-        dfifo_bus.buffer_sel.next = False
-        yield clock.posedge
 
-        dfifo_bus.write_enable.next = True
-        dfifo_bus.data_in.next = -1 and 0xFF
-        yield clock.posedge
-        dfifo_bus.write_enable.next = False
-        yield clock.posedge
-        assert dfifo_bus.fifo_empty
+        @instance
+        def tbstim():
+            """Set of inputs"""
+            print("start simulation")
+            dfifo_bus.buffer_sel.next = False
+            dfifo_bus.write_enable.next = False
+            dfifo_bus.read_req.next = False
+            yield reset_on_start(clock, reset)
+            assert dfifo_bus.fifo_empty
 
-        dfifo_bus.write_enable.next = True
-        dfifo_bus.data_in.next = 0xA1
-        yield clock.posedge
-        dfifo_bus.write_enable.next = False
-        yield clock.posedge
-        assert dfifo_bus.fifo_empty
-
-        dfifo_bus.write_enable.next = True
-        dfifo_bus.data_in.next = 0x11
-        yield clock.posedge
-        dfifo_bus.write_enable.next = False
-        yield clock.posedge
-
-        dfifo_bus.write_enable.next = True
-        dfifo_bus.data_in.next = 0x101
-        yield clock.posedge
-        dfifo_bus.write_enable.next = False
-        yield clock.posedge
-
-        for test_cases in range(64):
-            if test_cases < 28:
-                dfifo_bus.buffer_sel.next = False
-                yield clock.posedge
-            else:
-                dfifo_bus.buffer_sel.next = True
-                yield clock.posedge
+            dfifo_bus.buffer_sel.next = False
+            yield clock.posedge
 
             dfifo_bus.write_enable.next = True
-            dfifo_bus.data_in.next = test_cases
+            dfifo_bus.data_in.next = -1 and 0xFF
+            yield clock.posedge
+            dfifo_bus.write_enable.next = False
+            yield clock.posedge
+            assert dfifo_bus.fifo_empty
+
+            dfifo_bus.write_enable.next = True
+            dfifo_bus.data_in.next = 0xA1
+            yield clock.posedge
+            dfifo_bus.write_enable.next = False
+            yield clock.posedge
+            assert dfifo_bus.fifo_empty
+
+            dfifo_bus.write_enable.next = True
+            dfifo_bus.data_in.next = 0x11
             yield clock.posedge
             dfifo_bus.write_enable.next = False
             yield clock.posedge
 
-        dfifo_bus.read_req.next = True
-        yield clock.posedge
-        assert dfifo_bus.data_out and -1 == -1
-        dfifo_bus.read_req.next = False
+            dfifo_bus.write_enable.next = True
+            dfifo_bus.data_in.next = 0x101
+            yield clock.posedge
+            dfifo_bus.write_enable.next = False
+            yield clock.posedge
 
-        dfifo_bus.buffer_sel.next = True
-        yield clock.posedge
+            for test_cases in range(64):
+                if test_cases < 28:
+                    dfifo_bus.buffer_sel.next = False
+                    yield clock.posedge
+                else:
+                    dfifo_bus.buffer_sel.next = True
+                    yield clock.posedge
 
-        dfifo_bus.read_req.next = True
-        yield clock.posedge
-        assert dfifo_bus.data_out == 0xA1
-        dfifo_bus.read_req.next = False
-
-        dfifo_bus.buffer_sel.next = True
-        yield clock.posedge
-
-        dfifo_bus.read_req.next = True
-        yield clock.posedge
-        assert dfifo_bus.data_out == 0x11
-        dfifo_bus.read_req.next = False
-
-        dfifo_bus.buffer_sel.next = True
-        yield clock.posedge
-
-        dfifo_bus.read_req.next = True
-        yield clock.posedge
-        assert dfifo_bus.data_out == 0x101
-        dfifo_bus.read_req.next = False
-
-        for test_cases in range(64):
-            if test_cases < 28:
-                dfifo_bus.buffer_sel.next = True
+                dfifo_bus.write_enable.next = True
+                dfifo_bus.data_in.next = test_cases
                 yield clock.posedge
-            else:
-                dfifo_bus.buffer_sel.next = False
+                dfifo_bus.write_enable.next = False
                 yield clock.posedge
 
             dfifo_bus.read_req.next = True
             yield clock.posedge
-            assert dfifo_bus.data_out == test_cases
+            assert dfifo_bus.data_out and -1 == -1
             dfifo_bus.read_req.next = False
 
-        yield clock.posedge
+            dfifo_bus.buffer_sel.next = True
+            yield clock.posedge
 
-        assert dfifo_bus.fifo_empty
+            dfifo_bus.read_req.next = True
+            yield clock.posedge
+            assert dfifo_bus.data_out == 0xA1
+            dfifo_bus.read_req.next = False
 
-        raise StopSimulation
+            dfifo_bus.buffer_sel.next = True
+            yield clock.posedge
 
-    return dut, tbclock, tbstim
+            dfifo_bus.read_req.next = True
+            yield clock.posedge
+            assert dfifo_bus.data_out == 0x11
+            dfifo_bus.read_req.next = False
+
+            dfifo_bus.buffer_sel.next = True
+            yield clock.posedge
+
+            dfifo_bus.read_req.next = True
+            yield clock.posedge
+            assert dfifo_bus.data_out == 0x101
+            dfifo_bus.read_req.next = False
+
+            for test_cases in range(64):
+                if test_cases < 28:
+                    dfifo_bus.buffer_sel.next = True
+                    yield clock.posedge
+                else:
+                    dfifo_bus.buffer_sel.next = False
+                    yield clock.posedge
+
+                dfifo_bus.read_req.next = True
+                yield clock.posedge
+                assert dfifo_bus.data_out == test_cases
+                dfifo_bus.read_req.next = False
+
+            yield clock.posedge
+
+            assert dfifo_bus.fifo_empty
+
+            raise StopSimulation
+
+        return inst,inst_clock, tbstim
+
+    inst_dbuf = bench_doublebuffer()
+    inst_dbuf.config_sim(trace=False)
+    inst_dbuf.run_sim()
+
+def test_doublebuffer_conversion():
+
+    @block
+    def bench_doublebuffer_conversion():
+        """ test bench """
+        buffer_constants = BufferConstants(20, 64)
+        clock = Signal(bool(0))
+        reset = ResetSignal(0, active=1, async=True)
+        dfifo_bus = DoubleFifoBus(buffer_constants.width)
+        inst = rledoublefifo(buffer_constants, reset, clock, dfifo_bus)
+        inst_clock = tbclock(clock)
 
 
-def testbench():
-    """Run Simulation from here"""
+        inst_reset = resetonstart(clock, reset)
 
-    inst = test()
-    inst.config_sim(trace=False)
-    inst.run_sim()
+        @instance
+        def tbstim():
 
-    # verify.simulator = 'iverilog'
-    # assert test().verify_convert() == 0
+            yield clock.posedge
+            print ("Conversion done!!")
+            raise StopSimulation
 
-    # verify.simulator = 'ghdl'
-    # assert test().verify_convert() == 0
+        return tbstim, inst, inst_clock, inst_reset
 
-if __name__ == '__main__':
-    testbench()
+    verify.simulator = 'iverilog'
+    assert bench_doublebuffer_conversion().verify_convert() == 0
