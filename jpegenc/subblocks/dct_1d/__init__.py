aab4092c54c8b60e94de9dadc5c425a11b7e21e9
mkatsimpris <merkourioskatsimpris@gmail.com>
2016-06-22 16:23:14 +0300

    added __init__.py

---
+++ jpegenc/subblocks/dct_1d/__init__.py
@@ -0,0 +1,4 @@
+from .dct_1d import dct_1d_transformation
+from .dct_1d import dct_1d
+
+__all__ = ["dct_1d_transformation", "dct_1d"]
---
+++ jpegenc/subblocks/dct_2d/__init__.py
@@ -0,0 +1,4 @@
+from .dct_2d import dct_2d_transformation
+from .dct_2d import dct_2d
+
+__all__ = ["dct_2d_transformation", "dct_2d"]
