cfefb7ada81e86faec99fe3c544e96e765bbdacd
mkatsimpris <merkourioskatsimpris@gmail.com>
2016-07-15 17:13:40 +0300

    minor changes

--- jpegenc/subblocks/common/reusable_blocks.py
+++ jpegenc/subblocks/common/reusable_blocks.py
@@ -5,7 +5,16 @@ from myhdl import Signal, intbv, block, always_comb
 
 @block
 def assign(a, b):
+    """assign `a` to `b`
 
+    a = b
+
+    Arguments:
+        a (SignalType): the signal to assign to
+        b (SignalType): the signal to assign from
+
+    myhdl convertible
+    """
     @always_comb
     def assign():
             a.next = b
@@ -15,7 +24,14 @@ def assign(a, b):
 
 @block
 def assign_array(a, b):
+    """assign one list-of-signals (`b`) to another (`a`)
 
+    Arguments:
+        a (list): list-of-signals to assign to (output)
+        b (list): list-of-signals (input)
+
+    myhdl convertible
+    """
     assert isinstance(a, list)
     assert isinstance(b, list)
     assert len(a) == len(b)
