0b3fc04d8bea317f692614b378b2d37f17dd4d82
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-07-16 12:55:16 +0530

    added all quantizer files

--- jpegenc/subblocks/__init__.py
+++ jpegenc/subblocks/__init__.py
@@ -5,8 +5,10 @@ from .color_converters import rgb2ycbcr
 from .dct import dct_2d
 from .rle import rlencoder
 from .quantizer import quantizer_top
+from .huffman import huffman
+from .bytestuffer import bytestuffer
 
 
 __all__ = [
-    'rgb2ycbcr', 'dct_2d', 'rlencoder', 'quantizer_top'
+    'rgb2ycbcr', 'dct_2d', 'rlencoder', 'quantizer_top', 'huffman', 'bytestuffer'
 ]
