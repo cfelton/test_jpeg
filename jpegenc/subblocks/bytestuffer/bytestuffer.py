ba72f2c6bd1d47c56757354e3e6e9ed4541de614
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-07-20 16:20:58 +0530

    added bytestuffer

--- jpegenc/subblocks/bytestuffer/bytestuffer.py
+++ jpegenc/subblocks/bytestuffer/bytestuffer.py
@@ -52,7 +52,6 @@ class BSOutputDataStream(object):
 @block
 def bytestuffer(
         clock, reset, bs_in_stream, bs_out_stream, bs_cntrl, num_enc_bytes):
-<<<<<<< HEAD
     """Byte stuffer checks for 0xFF byte and adds a 0xFF00 Byte
 
     Constants:
@@ -66,23 +65,6 @@ def bytestuffer(
     Returns:
     bs_out_stream : output interface to the byte stuffer
     num_enc_byte : number of bytes encoded to output RAM
-=======
-    """
-    Byte stuffer checks for 0xFF byte and adds a 0xFF00 Byte
-
-    Constants:
-
-    width_addr_out : maximum adress width of the output RAM
-    width_out      : width of the data in the ouput RAM
-
-    I/O Ports :
-
-    bs_in_stream  : input interface to the byte stuffer
-    bs_cntrl      : control interface to the byte stuffer
-    bs_out_stream : output interface to the byte stuffer
-    num_enc_byte  : number of bytes encoded to output RAM
->>>>>>> upstream/master
-
     """
 
     assert isinstance(bs_in_stream, BSInputDataStream)
