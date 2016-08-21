c6c0c2f92c71e871dc453fe3107372f89ff7078a
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-08-22 04:01:25 +0530

    edited init file

--- test/__init__.py
+++ test/__init__.py
@@ -20,7 +20,24 @@ from .test_zig_zag import test_zig_zag_conversion
 from .test_frontend_v2 import test_frontend
 from .test_frontend_v2 import test_frontend_conversion
 
+from .test_quantizer import test_quantizer
+from .test_quantizer import test_quant_conversion
+
+from .test_quantizer_core import test_quantizer_core
+from .test_quantizer_core import test_block_conversion
+
+from .test_divider import test_quantizer
+from .test_divider import test_quant_conversion
+
+from .test_rle import test_rle
+from .test_rle import test_rle_conversion
+
+from .test_rlecore import test_rle_core
+from .test_rlecore import test_rle_conversion
+
 __all__ = ['test_block_conversion', 'test_color_translation', 'test_color_translation_v2',
            'test_color_translation_conversion_v2', 'test_dct_1d', 'test_dct_1d_conversion',
            'test_dct_2d', 'test_dct_2d_conversion', 'test_zig_zag', 'test_zig_zag_conversion',
-           'test_frontend', 'test_frontend_conversion']
+           'test_frontend', 'test_frontend_conversion', 'tes_quantizer_core', 'test_quantizer',
+           'test_block_conversion', 'test_quant_conversion', 'test_rle', 'test_rlecore',
+           'test_rle_conversion']
